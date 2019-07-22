34174b26935f991ca9f31cb200dfcb17f678aa3c
Andre Pinto <anpint@microsoft.com>
2019-07-22 14:57:33 -0700

    playing around with quality of response improvements

--- ij-chatbot-src/Bots/QnABot.cs
+++ ij-chatbot-src/Bots/QnABot.cs
@@ -41,15 +41,44 @@ namespace Microsoft.BotBuilderSamples
             _logger.LogInformation("Calling QnA Maker");
 
             // The actual call to the QnA Maker service.
-            var response = await qnaMaker.GetAnswersAsync(turnContext);
+            // The response will contain all of the questions that provide the response.
+            var response = await qnaMaker.GetAnswersAsync(turnContext, new QnAMakerOptions { Top = 3 });
             if (response != null && response.Length > 0)
             {
-                await turnContext.SendActivityAsync(MessageFactory.Text(response[0].Answer), cancellationToken);
+                // If multiple responses are received, the first element will be the most confident.
+
+                // Append lack of confidence message if applicable.
+                if (response[0].Score < 0.5)
+                {
+                    // Answer may not apply at all
+                    await turnContext.SendActivityAsync(MessageFactory.Text("I couldn't find a good answer to your question. Here's the closest match:"), cancellationToken);
+                }
+                else if (response[0].Score < 0.7)
+                {
+                    // Express some uncertainty in the result
+                    await turnContext.SendActivityAsync(MessageFactory.Text("This seems related to what you asked:"), cancellationToken);
+                }
+
+                await turnContext.SendActivityAsync(MessageFactory.Text($"Question: {response[0].Questions[0]}" + System.Environment.NewLine + $"Answer: {response[0].Answer}"), cancellationToken);
+
+                // Handle the additional responses:
+                if (response.Length > 1)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text("I also found these topics:"), cancellationToken);
+                    for (int i = 1; i < response.Length; i++)
+                    {
+                        await turnContext.SendActivityAsync(MessageFactory.Text($"Question: {response[i].Questions[0]}" + System.Environment.NewLine + $"Answer: {response[i].Answer}"), cancellationToken);
+
+                    }
+                }
             }
             else
             {
                 await turnContext.SendActivityAsync(MessageFactory.Text("No QnA Maker answers were found."), cancellationToken);
             }
+
+            // TODOs: 
+            // - better formatting of responses
         }
 
         private string GetHostname()
