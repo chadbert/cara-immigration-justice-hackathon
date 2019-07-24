9f73d87a785cd98bef866aea1932ed31e85cf9af
Andre Pinto <anpint@microsoft.com>
2019-07-23 17:27:17 -0700

    add the mock interview bot

---
+++ MockInterviewBot/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=ij-interview-bot-Web-Deploy.pubxml -p:Password=LyGvJrl38wAst87sFmrSmvXbQTXhfSd65aStYojxEaTvG2YssazayTlxwMsX
+
